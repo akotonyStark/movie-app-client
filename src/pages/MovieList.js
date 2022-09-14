d0be67925fd0527ad2f7bc038498fecba19fe00f
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-09-14 11:15:36 +0000

    setup custom fetch hook

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,14 +1,13 @@
 import { Grid } from '@mui/material'
 import React, { useState } from 'react'
 import MovieItem from '../components/MovieItem'
+import useFetch from '../customHooks/useFetch'
 
 const MovieList = () => {
-  const [movies, setMovies] = useState([
-    {
-      name: 'some',
-      release_year: 1990,
-    },
-  ])
+  const url = 'http://localhost:3001/movies'
+  const { data, isLoading, isError } = useFetch(url)
+  console.log(data)
+  const [movies, setMovies] = useState([])
   return (
     <Grid container spacing={2} sx={{ display: 'flex' }}>
       {movies?.map((movie, index) => {
