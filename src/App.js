b36987329c1759abe702a022d4c0fc785909fde3
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-09-14 10:17:47 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MovieList from './pages/MovieList'
+import MovieDetails from './pages/MovieDetails'
+import NavHeader from './components/NavHeader'
+function App() {
+  return (
+    <div className='App'>
+      <NavHeader />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<MovieList />} />
+          <Route path='/details' element={<MovieDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/MovieDetails.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MovieDetails = () => {
+  return <div>MovieDetais</div>
+}
+
+export default MovieDetails
